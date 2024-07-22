49c6e481f0a4fa1fe8e5a49b7bb41b3b723bddcc
Valiant <vezabuku09@gmail.com>
2024-07-22 20:49:48 +0300

    Final Commit

--- src/api/config.py
+++ src/api/config.py
@@ -4,12 +4,13 @@ ONE_DAY_SEC = 24*60*60
 
 ONE_WEEK_SEC = ONE_DAY_SEC*7
 
-XGBOOST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/xgboost.joblib"
+XGBOOST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI/main/dev/models/xgboost.joblib"
 
-RANDOM_FOREST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/random_forest.joblib"
+RANDOM_FOREST_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI/main/dev/models/random_forest.joblib"
 
-ENCODER_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI-Collabo/develop/dev/models/encoder.joblib"
+ENCODER_URL = "https://raw.githubusercontent.com/valiantezabuku/Building-Machine-Learning-API-s-with-FastAPI/main/dev/models/encoder.joblib"
 
 BASE_DIR = './'  # Where Unicorn server runs from
 
 ENV_PATH = os.path.join(BASE_DIR, 'src/api/.env')
+
